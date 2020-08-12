03577aa5f6dab0424066ce36d861e35c8c7db110
ben4code <nnaemeka.obioha@gmail.com>
2020-08-12 17:48:38 +0100

    Initial commit with cloudinay, mongodb-dev and mongodb-prod installed

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bd66999be7c542d15b82c4050b51af5d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd68d8544-63a1-4b0e-abef-a458b6583b6c'
+};
